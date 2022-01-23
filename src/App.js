28bd9ac16d5fabbd3484dc050acbba181c36a044
Lucas Valle <lucasfvalle@gmail.com>
2022-01-23 01:59:58 -0300

    Clone Netflix 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Tmdb from './Tmdb';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  useEffect(()=>{
+    const loadAll = async () => {
+      // Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando filme em destaque (featured)
+      let originals = list.filter(i=>i.slug == 'originals'); // Filtro para pegar somente originais netflix através do slug
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1)); // "Sorteando" um filme/serie aleatório
+
+      let chosen = originals[0].items.results[randomChosen]; // Registrando o escolhido
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData (chosenInfo);
+    }
+    loadAll();
+  }, []);
+
+  return(
+    <div className="page">
+    
+    {featuredData &&
+      <FeaturedMovie item={featuredData} />
+    }
+
+    <section className="lists">
+      {movieList.map((item, key) => (
+       <MovieRow key={key} title={item.title} items={item.items} />
+      ))
+
+      }
+    </section>
+    </div>
+  )
+}
